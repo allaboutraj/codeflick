daa3ee9494bce196f65a0cc3b433cd32e9dfb880
allaboutraj <rajgupta9507321@gmail.com>
2024-03-31 02:58:37 +0530

    Added Feature routing structure for versioned  API for Problems

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 
 
 const {PORT} =  require('./config/server.config');
-
+const apiRouter = require('./routes')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
+
+app.use('./api', apiRouter);
 
 app.get('/ping', (req, res) => {
     return res.json({message: 'Ping Service is alive'});
