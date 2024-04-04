db804683b8d3375a80771705e4b2f8aa97dd6cec
allaboutraj <rajgupta9507321@gmail.com>
2024-04-05 01:49:48 +0530

    Feature - Fixed the some variable naming for api

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,4 +1,4 @@
-// const {StatusCodes} = require('http-status-codes');
+const {StatusCodes} = require('http-status-codes');
 const NotImplemented = require('../errors/notImplemented.error');
 const { ProblemService } = require('../services')
 const { ProblemRepository} = require('../repositories')
@@ -13,12 +13,12 @@ function pingProblemController(req, res) {
 async function addProblem(req, res, next) {
     try {
         console.log("Incoming Request Body", req.body);
-        const newProblem = await problemService.createProblem(req.body);
+        const newproblem = await problemService.createProblem(req.body);
         return res.status(StatusCodes.CREATED).json({
             success: true,
             message : 'Successfully Created a new Problem',
             error: {},
-            data: newProblem 
+            data: newproblem 
         })
     } catch (error) {
         next(error);
